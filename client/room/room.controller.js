9a3642e5d9e9deb9a3ced4efafe7459aced1a567
Thilun <Thilun003@gmail.com>
2017-03-27 13:46:57 +0200

    integration socket base

--- client/room/room.controller.js
+++ client/room/room.controller.js
@@ -3,17 +3,59 @@
 
 angular.module('rpgApp').controller('RoomController', function ($scope, mySocket) {
 	console.log('room')
-	console.log(mySocket)
 	var vm = $scope
+	vm.isConnected = false;
+	vm.player = {}
 	// var socket = io({transports: ['websocket'], upgrade: false});
 
+	var canvas = document.getElementById("cnv").getContext("2d");
+
+	canvas.font = "30px Arial";
 
 	vm.joinRoom = function (){
 		console.log('Trying to join')
 		mySocket.emit('newPlayerJoinning')
 	}
 
+	vm.leaveRoom = function (){
+		console.log('Trying to leave')
+		mySocket.emit('PlayerLeaving', vm.player)
+	}
+
 	mySocket.on('newPlayerJoinned', function(data){
-		console.log('welcome', data)
+		vm.isConnected = true;
+		vm.player = data
+		console.log('Welcome', data)
 	})
+
+	mySocket.on('playerLeft', function(data){
+		vm.isConnected = false;
+		console.log('Bye', data)
+	})
+
+	mySocket.on('newPositions',function(data){
+		console.log('fill')
+		canvas.clearRect(0,0,1000,600)
+
+		for(var i = 0; i < data.length; i++) {
+			canvas.fillText(data[i].id.slice(0,6), data[i].x, data[i].y);
+			/*player.update();
+	  	player.render();*/
+	  }
+	})
+
+	document.onkeydown = function(event){
+	 if(event.keyCode === 37) mySocket.emit('keyPress', {inputId : 37, state : true})
+	 if(event.keyCode === 39) mySocket.emit('keyPress', {inputId : 39, state : true})
+	 if(event.keyCode === 38) mySocket.emit('keyPress', {inputId : 38, state : true})
+	 if(event.keyCode === 40) mySocket.emit('keyPress', {inputId : 40, state : true})
+	}
+
+	document.onkeyup = function(event){
+	 if(event.keyCode === 37) mySocket.emit('keyPress', {inputId : 37, state : false})
+	 if(event.keyCode === 39) mySocket.emit('keyPress', {inputId : 39, state : false})
+	 if(event.keyCode === 38) mySocket.emit('keyPress', {inputId : 38, state : false})
+	 if(event.keyCode === 40) mySocket.emit('keyPress', {inputId : 40, state : false})
+	}
+
 })
--- server/socket.js
+++ server/socket.js
@@ -1,22 +1,78 @@
 'use strict';
 
 var path = require('path');
+var uuid = require('uuid');
+var clientsList = {};
+var playersList = {};
+
+function Player(id){
+  var self = {
+    id : id,
+    x : 250,
+    y : 250,
+    pressingLeft : false,
+    pressingRight : false,
+    pressingUp : false,
+    pressingDown : false,
+    speed : 10
+  }
+
+  self.updatePosition = function () {
+    if(self.pressingLeft) this.x -= self.speed;
+    if(self.pressingRight) this.x += self.speed;
+    if(self.pressingUp) this.y -= self.speed;
+    if(self.pressingDown) this.y += self.speed;
+  }
+
+  return self
+}
 
 module.exports.default = function(io) {
   io.on('connection', function (client) {
 
-    console.log('co')
+  console.log('co')
+  client.id = uuid.v4();
+  clientsList[client.id] = client;
+  var player = {}
+
     client.on('newPlayerJoinning', function (data) {
-      setTimeout(function(){
-        client.emit('newPlayerJoinned', { hello: 'world' });
-      },3000)
+      console.log('player is joinning')
+      player = Player(client.id)
+      playersList[client.id] = player;
+      client.emit('newPlayerJoinned', player);
     });
 
+    client.on('PlayerLeaving', function (data) {
+        delete playersList[client.id];
+        client.emit('playerLeft', data.id);
+    });
 
+  client.on('keyPress', function(data){
+    if(data.inputId === 37) player.pressingLeft = data.state;
+    if(data.inputId === 39) player.pressingRight = data.state;
+    if(data.inputId === 38) player.pressingUp = data.state;
+    if(data.inputId === 40) player.pressingDown = data.state;
+  })
 
     client.on('disconnect',function(client){
       console.log('deco')
+      delete clientsList[client.id];
+      delete playersList[client.id];
     })
 
   });
 }
+
+setInterval(function(){
+  var pack = [];
+  for(var i in playersList){
+    var player = playersList[i]
+    player.updatePosition()
+    pack.push({id:player.id,x:player.x, y:player.y})
+  }
+
+  for(var i in clientsList){
+    var client = clientsList[i];
+    client.emit('newPositions', pack);
+  }
+},1000/25)
